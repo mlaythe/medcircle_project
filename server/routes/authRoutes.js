3d0ca2af59b10141032f8e96c7fb4d8f0509690f
Michael Laythe <mrlaythe24@aol.com>
2016-12-06 13:20:44 -0800

    any routes put here must have proper authentication in order to consume endpoints

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const apiRoutes = require('./apiRoutes');
+const config = require('config');
+
+const authRoutes = module.exports = express.Router();
+
+authRoutes.use((req, res, next) => {
+  return req.get('Authorization') === config.get('bearer-token') ? next() : res.status(401).send('Missing proper authentication.');
+});
+
+authRoutes.use(apiRoutes);
