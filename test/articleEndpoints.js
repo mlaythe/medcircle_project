6f339543b71cc13068c5639fe30b9c87262a7f29
Michael Laythe <mrlaythe24@aol.com>
2016-12-06 15:20:07 -0800

    should respond with 401 when no token provided, and add test to see if /articles sends back all articles

--- test/articleEndpoints.js
+++ test/articleEndpoints.js
@@ -1,21 +1,50 @@
-const { expect } = require('chai').expect;
+const { expect } = require('chai');
 const app = require('../server/server');
 const request = require('supertest-as-promised')(app);
 const config = require('config');
+const token = config.get('bearer-token');
 
 describe('oAuth bearer token', () => {
 
   it('should respond with 200 when using correct token', (done) => {
-    request.get('/api')
-    .set('Authorization', config.get('bearer-token'))
+    request.get('/api/1.0')
+    .set('Authorization', token)
     .expect(200)
     .end(done);
   });
 
   it('should respond with 401 when using incorrect token', (done) => {
-    request.get('/api')
+    request.get('/api/1.0')
     .set('Authorization', 'foobar')
     .expect(401)
     .end(done);
+  });
+
+  it('should respond with 401 when no token provided', (done) => {
+    request.get('/api/1.0')
+    .set('Authorization', 'foobar')
+    .expect(401)
+    .end(done);
+  });
+});
+
+describe('Article GET', () => {
+
+  it('should respond with all articles when no ID specified', (done) => {
+    request.get('/api/1.0/articles')
+    .set('Authorization', token)
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end((err, res) => {
+      const result = JSON.parse(res.text);
+  
+      try {
+        expect(result.length).to.eql(10);
+      } catch(err) {
+        throw new Error(err);
+      }
+      
+      done();
+    });
   });
 });
