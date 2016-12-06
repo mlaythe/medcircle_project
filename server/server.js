7584e6200f100e75bd6e8cd60bddf9221a9bdee9
Michael Laythe <mrlaythe24@aol.com>
2016-12-06 13:19:21 -0800

    add bodyParser, and add authenticated routes to middleware

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+
+app.use(bodyParser.json());
+
+app.use(authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
