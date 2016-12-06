372e3aa9ca3f14b8729b0100a992f7607df3110e
Michael Laythe <mrlaythe24@aol.com>
2016-12-06 15:20:27 -0800

    requests to /articles sends back all articles from db

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const { getArticles } = require('../Articles/articleController');
 
 const apiRoutes = module.exports = express.Router();
 
-apiRoutes.get('/', (req, res) => {
-  console.log('inside of /')
+apiRoutes.get('/:version', (req, res) => {
   res.status(200).send('Successful');
 });
+
+apiRoutes.get('/:version/articles', getArticles);
