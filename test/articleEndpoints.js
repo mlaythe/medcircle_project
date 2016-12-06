59fed710a6ebef013433cefe7e1ddb3482b660f4
Michael Laythe <mrlaythe24@aol.com>
2016-12-06 13:20:11 -0800

    basic skeleton for testing & add test that responds with 200 when properly authenticated

---
+++ test/articleEndpoints.js
@@ -0,0 +1,14 @@
+const { expect } = require('chai').expect;
+const app = require('../server/server');
+const request = require('supertest-as-promised')(app);
+const config = require('config');
+
+describe('oAuth bearer token', () => {
+
+  it('should respond with 200 when using correct token', (done) => {
+    request.get('/')
+    .set('Authorization', config.get('bearer-token'))
+    .expect(200)
+    .end(done);
+  });
+});
