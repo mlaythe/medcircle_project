12ffbb5cf785a97d273c86e98064b2464149057e
Michael Laythe <mrlaythe24@aol.com>
2016-12-06 18:35:13 -0800

    remove /:version endpoint

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -3,8 +3,4 @@ const articles1 = require('../1.0/articles');
 
 const apiRoutes = module.exports = express.Router();
 
-apiRoutes.get('/:version', (req, res) => {
-  res.status(200).send('Successful');
-});
-
 apiRoutes.use('/1.0', articles1);
