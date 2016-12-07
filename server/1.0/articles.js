73e79e53dae987d601e53bfbfc6ac0f487cf8da9
Michael Laythe <mrlaythe24@aol.com>
2016-12-06 18:11:56 -0800

    move article endpoints to specific version directory for cleaner setup

---
+++ server/1.0/articles.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const {  getArticles, updateArticle, deleteArticle  } = require('../Articles/articleController');
+
+const articles = module.exports = express.Router();
+
+articles.get('/articles', getArticles);
+articles.get('/articles/:id', getArticles);
+articles.put('/articles/:id', updateArticle);
+articles.delete('/articles/:id', deleteArticle);
