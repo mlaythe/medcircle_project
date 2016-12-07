94a8e8df40ed625b77b91cab5069251bd704cceb
Michael Laythe <mrlaythe24@aol.com>
2016-12-06 18:35:32 -0800

    GET request to root path sends instructions back to clinet

--- server/1.0/articles.js
+++ server/1.0/articles.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const { getArticles, updateArticle, deleteArticle } = require('../Articles/articleController');
+const description = require('./description');
 
 const articles = module.exports = express.Router();
 
+articles.get('/', (req, res) => {
+  res.send(description);
+});
 articles.get('/articles', getArticles);
 articles.get('/articles/:id', getArticles);
 articles.put('/articles/:id', updateArticle);
