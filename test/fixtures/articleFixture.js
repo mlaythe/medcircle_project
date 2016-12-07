e867c3dcb488d1efe82fbf4db8801cec53b6a696
Michael Laythe <mrlaythe24@aol.com>
2016-12-06 17:12:59 -0800

    create article fixture, so we don't pollute database

---
+++ test/fixtures/articleFixture.js
@@ -0,0 +1,10 @@
+module.exports = {
+  id: 24,
+  title: 'Testing all the things',
+  summary: 'Quae natus aliquid nesciunt aperiam veniam non molestiae repudiandae qui. Est saepe doloribus eos. Libero nulla voluptas voluptatibus sequi et laboriosam dolores enim veniam.',
+  media_url: 'https://medcircle-coding-project.s3.amazonaws.com/api/articles/24.jpg',
+  published_at: '2016-08-28T07:00:00.000Z',
+  likes_count: 240000,
+  author_name: 'Michael Laythe',
+  author_icon_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+};
