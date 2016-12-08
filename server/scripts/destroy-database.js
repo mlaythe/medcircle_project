c20f2b4a624e1849e4f0abd57897bc65d1128e4b
Michael Laythe <mrlaythe24@aol.com>
2016-12-07 21:38:29 -0800

    destroy connection pool once script is done running

--- server/scripts/destroy-database.js
+++ server/scripts/destroy-database.js
@@ -1,10 +1,28 @@
-const { bookshelf } = require('../database/database');
+const { knex } = require('../database/database');
 
-bookshelf.knex.schema.dropTable('articles')
-.then(() => {
-  console.log('Successfully dropped articles table from database!');
-  process.exit(0);
-})
-.catch((err) => {
-  throw new Error(err);
+knex.schema.hasTable('articles').then((exists) => {
+  if (exists) {
+    knex.schema.dropTable('articles')
+    .then(() => {
+      console.log('Successfully dropped articles table from database!');
+      return knex.destroy();
+    })
+    .then(() => {
+      console.log('Pool successfully cleared.');
+      process.exit(0);
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
+  } else {
+    knex.destroy()
+    .then(() => {
+      console.log('Pool successfully cleared. Articles table does not exist.');
+      process.exit(0);
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
+  }
 });
+
--- server/scripts/prepare-database.js
+++ server/scripts/prepare-database.js
@@ -5,41 +5,95 @@ const config = require('config');
 const articlesURL = config.get('articles-url');
 const Promise = require('bluebird');
 
-knex.schema.createTableIfNotExists('articles', article => {
-  article.integer('id').primary();
-  article.string('title');
-  article.string('summary');
-  article.string('media_url');
-  article.date('published_at');
-  article.integer('likes_count');
-  article.string('author_name');
-  article.string('author_icon_url');
-})
-.then(() => {
-  request(articlesURL, (err, res, body) => {
-    if (err) throw new Error(err);
+knex.schema.hasTable('articles').then((exists) => { 
+  if (exists) {
+    knex.schema.dropTable('articles').then((exists) => {
 
-    const articles = JSON.parse(body);
-    const promises = articles.map((article) => {
-      // destructure article object and add each property to data object
-      // use data object to write new Article to db
-      const { id, title, summary, media_url, published_at, likes_count, author: { name: author_name, icon_url: author_icon_url } } = article;
-      const data = { id, title, summary, media_url, published_at, likes_count, author_name, author_icon_url };
+      knex.schema.createTable('articles', article => {
+        article.integer('id').primary();
+        article.string('title');
+        article.string('summary');
+        article.string('media_url');
+        article.date('published_at');
+        article.integer('likes_count');
+        article.string('author_name');
+        article.string('author_icon_url');
+      })
+      .then(() => {
+        request(articlesURL, (err, res, body) => {
+          if (err) throw new Error(err);
 
-      return Article.forge(data).save(null, { method: 'insert' });
+          const articles = JSON.parse(body);
+          const promises = articles.map((article) => {
+            // destructure article object and add each property to data object
+            // use data object to write new Article to db
+            const { id, title, summary, media_url, published_at, likes_count, author: { name: author_name, icon_url: author_icon_url } } = article;
+            const data = { id, title, summary, media_url, published_at, likes_count, author_name, author_icon_url };
+
+            return Article.forge(data).save(null, { method: 'insert' });
+          });
+
+          // need Promise.all, because bookshelf doesn't expose a bulk write method, so we have to resolve every write to the db ourselves
+          Promise.all(promises)
+          .then(() => {
+            console.log('Successfully prepared the database!');
+            return knex.destroy();
+          })
+          .then(() => {
+            console.log('Pool successfully cleared');
+            process.exit(0);
+          })
+          .catch((error) => {
+            throw new Error(error);
+          });
+        });
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
     });
-
-    // need Promise.all, because bookshelf doesn't expose a bulk write method, so we have to resolve every write to the db ourselves
-    Promise.all(promises)
-    .then(() => {
-      console.log('Successfully prepared the database!');
-      process.exit(0);
+  } else {
+    knex.schema.createTable('articles', article => {
+      article.integer('id').primary();
+      article.string('title');
+      article.string('summary');
+      article.string('media_url');
+      article.date('published_at');
+      article.integer('likes_count');
+      article.string('author_name');
+      article.string('author_icon_url');
     })
-    .catch((error) => {
-      throw new Error(error);
+    .then(() => {
+      request(articlesURL, (err, res, body) => {
+        if (err) throw new Error(err);
+
+        const articles = JSON.parse(body);
+        const promises = articles.map((article) => {
+          // destructure article object and add each property to data object
+          // use data object to write new Article to db
+          const { id, title, summary, media_url, published_at, likes_count, author: { name: author_name, icon_url: author_icon_url } } = article;
+          const data = { id, title, summary, media_url, published_at, likes_count, author_name, author_icon_url };
+
+          return Article.forge(data).save(null, { method: 'insert' });
+        });
+
+        // need Promise.all, because bookshelf doesn't expose a bulk write method, so we have to resolve every write to the db ourselves
+        Promise.all(promises)
+        .then(() => {
+          console.log('Successfully prepared the database!');
+          return knex.destroy();
+        })
+        .then(() => {
+          console.log('Pool successfully cleared');
+          process.exit(0);
+        })
+        .catch((error) => {
+          throw new Error(error);
+        });
+      });
+    })
+    .catch((err) => {
+      throw new Error(err);
     });
-  });
-})
-.catch((err) => {
-  throw new Error(err);
-});
+  }
+}); 
