5a319b4231a5d32e2ae0194d50f03ff7650e101f
Michael Laythe <mrlaythe24@aol.com>
2016-12-07 21:38:10 -0800

    remove custom pooling and add acquireConnectionTimeout to debug for future connection pooling issues

--- server/database/database.js
+++ server/database/database.js
@@ -4,10 +4,7 @@ const dbKey = config.get('db-key');
 const knex = require('knex')({
   client: 'pg',
   connection: dbKey,
-  pool: {
-    min: 1,
-    max: 7,
-  },
+  acquireConnectionTimeout: 10000,
 });
 
 const bookshelf = require('bookshelf')(knex);
